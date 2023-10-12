e94a7e433ac635262ea10d94a05011ab325111aa
Danial Bleile <danbleile@gmail.com>
2023-10-12 15:32:08 -0700

    feat: add directory query

--- classes/directories.php
+++ classes/directories.php
@@ -4,31 +4,137 @@ class Directories {
 
 	private static $post_type = 'wsu_directory';
 
-	public static function get_directory_people_ids( $directory_id, $include_inherited = true ) {
 
-		$people_ids = array();
+	public static function get_directories( $directory_id, $args = array() ) {
 
-		$directories = array( $directory_id );
+		$default_args = array(
+			'include_parent' => true,
+			'inherit'        => 'all', // supports all|children
+		);
 
-		if ( $include_inherited ) {
+		self::parse_args( $default_args, $args );
 
-			$child_directories = self::get_child_directory_ids( $directory_id );
+		$directories = array();
+
+		if ( $args['include_parent'] ) {
+
+			$directories[] = self::get_directory( $directory_id );
+
+		}
+
+		$child_directories = self::get_child_directories( $directory_id, $args );
+
+		if ( ! empty( $child_directories ) ) {
 
 			$directories = array_merge( $directories, $child_directories );
 
 		}
 
-		$directories = array_filter( array_unique( $directories ) );
+		return $directories;
+
+	}
+
+
+	public static function get_directory( $directory, $args = array() ) {
+
+
+		if ( ! $directory instanceof \WP_Post ) {
+
+			if ( is_numeric( $directory ) ) {
+
+				$directory = get_post( intval( $directory ) );
+
+			} else {
+
+				$directory = false;
+
+			}
+		}
+
+		if ( $directory ) {
+
+			$default_args = array(
+				'fields' => array(
+					'slug',
+					'title',
+					'id',
+					'peopleIDs',
+					'path',
+					'editLink',
+				),
+			);
+
+			self::parse_args( $default_args, $args );
+
+			$people_array = self::get_people_ids_from_directory( $directory->ID );
+
+			$directory_array = array(
+				'slug'       => $directory->post_name,
+				'title'      => $directory->post_title,
+				'id'         => $directory->ID,
+				'peopleIDs'  => ( in_array( 'peopleIDs', $args['fields'], true ) ) ? self::get_people_ids_from_directory( $directory->ID ) : array(),
+				'path'       => ( in_array( 'path', $args['fields'], true ) ) ? self::get_directory_path( $directory->ID ) : array(),
+				'editLink'   => ( in_array( 'editLink', $args['fields'], true ) ) ? admin_url() . '/post.php?post=' . $directory->ID . '&action=edit' : '',
+			);
+
+			// Unset array keys we didn't ask for
+			foreach ( $directory_array as $key => $value ) {
+
+				if ( ! in_array( $key, $args['fields'], true ) ) {
+
+					unset( $directory_array[ $key ] );
+
+				}
+			}
+		}
+
+		return $directory_array;
+
+	}
+
+
+	public static function get_people_directory_index( $directories ) {
+
+		$people_array = array();
 
 		foreach ( $directories as $directory ) {
 
-			$directory_people_ids = self::get_people_ids( $directory );
+			if ( is_array( $directory['peopleIDs'] ) ) {
 
-			$people_ids = array_merge( $people_ids, $directory_people_ids );
+				foreach ( $directory['peopleIDs'] as $person_id ) {
 
+					if ( ! array_key_exists( $person_id, $people_array ) ) {
+
+						$people_array[ $person_id ] = array();
+
+					}
+
+					$people_array[ $person_id ][] = array(
+						'name' => $directory['title'],
+						'slug'  => $directory['slug'],
+					);
+				}
+			}
 		}
 
-		return $people_ids;
+		return $people_array;
+
+	}
+
+
+	public static function get_directory_people_ids( $directory_id, $args = array() ) {
+
+		$default_args = array(
+			'inherit' => 'all',
+		);
+
+		self::parse_args( $default_args, $args );
+
+		$people_ids = array();
+
+		$directories = self::get_directories( $directory_id, $args );
+
+		return self::get_people_ids_from_directories( $directories );
 
 	}
 
@@ -41,7 +147,7 @@ class Directories {
 
 		foreach ( $child_directories as $directory ) {
 
-			$directory_ids[] = $directory->ID;
+			$directory_ids[] = $directory['id'];
 
 		}
 
@@ -50,21 +156,53 @@ class Directories {
 	}
 
 
-	public static function get_child_directories( $directory_id ) {
+	public static function get_child_directories( $directory_id, $args = array() ) {
 
-		$directory_posts = get_pages(
-			array(
-				'child_of'  => $directory_id,
-				'post_type' => self::$post_type,
-			)
+		$default_args = array(
+			'inherit'        => 'all', // supports all|children
 		);
 
-		return ( ! empty( $directory_posts ) ) ? $directory_posts : array();
+		self::parse_args( $default_args, $args );
+
+		if ( 'none' === $args['inherit'] ) {
+
+			return array();
+
+		}
+
+		$directory_array = array();
+
+		$directory_query = array(
+			'post_type' => self::$post_type,
+		);
+
+		switch ( $args['inherit'] ) {
+
+			case 'all':
+				$directory_query['child_of'] = $directory_id;
+				break;
+			case 'children':
+				$directory_query['parent'] = $directory_id;
+				break;
+		}
+
+		$directory_posts = get_pages( $directory_query );
+
+		if ( ! empty( $directory_posts ) ) {
+
+			foreach ( $directory_posts as $directory ) {
+
+				$directory_array[] = self::get_directory( $directory, $args );
+
+			}
+		}
+
+		return $directory_array;
 
 	}
 
 
-	public static function get_people_ids( $directory_id ) {
+	public static function get_people_ids_from_directory( $directory_id ) {
 
 		$people = get_post_meta( $directory_id, 'wsu_people', true );
 
@@ -78,6 +216,64 @@ class Directories {
 
 			return array();
 
+		}
+
+	}
+
+	public static function get_people_ids_from_directories( $directories ) {
+
+		$people_ids = array();
+
+		foreach ( $directories as $directory ) {
+
+			$people_ids = array_merge( $people_ids, $directory['peopleIDs'] );
+
+		}
+
+		return $people_ids;
+
+	}
+
+
+	public static function get_directory_path( $directory_id ) {
+
+		$directory_path_array = array();
+
+		self::directory_parent_recursive( $directory_path_array, $directory_id );
+
+		return $directory_path_array;
+
+	}
+
+
+	public function directory_parent_recursive( &$directory_path, $directory_id ) {
+
+		$parent_post = get_post_parent( $directory_id );
+
+		if ( $parent_post ) {
+
+			$directory_path[] = array( 
+				'title' => $parent_post->post_title,
+				'post_id' => $parent_post->ID,
+				'editLink'   => admin_url() . '/post.php?post=' . $directory->ID . '&action=edit',
+			);
+
+			self::directory_parent_recursive( $directory_path, $parent_post->ID );
+
+		}
+
+	}
+
+
+	protected static function parse_args( $default_args, &$args ) {
+
+		foreach ( $default_args as $key => $default ) {
+
+			if ( ! array_key_exists( $key, $args ) ) {
+
+				$args[ $key ] = $default;
+
+			}
 		}
 
 	}
---
+++ includes/directory-query.php
@@ -0,0 +1,158 @@
+<?php namespace WSUWP\Plugin\People\API;
+
+class Directory_Query {
+
+	/**
+	 * create_organization
+	 *
+	 * @param \WP_REST_Request $request
+	 * @return \WP_REST_Response
+	 */
+	public static function directory_path( \WP_REST_Request $request ) {
+
+		$params         = $request->get_body_params();
+		$directory_id   = $request['directory'] ? sanitize_text_field( $request['directory'] ) : false;
+		$directory_path = array();
+
+		if ( $directory_id ) {
+
+			$directory_path = Directories::get_directory_path( $directory_id );
+
+		}
+
+		$directory_path = array_reverse( $directory_path );
+
+		return new \WP_REST_Response( $directory_path, 200 );
+
+	}
+
+	public static function register_api_endpoints() {
+
+		register_rest_route(
+			'peopleapi/v1',
+			'/editor/directory/path',
+			array(
+				'methods' => \WP_REST_Server::READABLE,
+				'callback' => array( __CLASS__, 'directory_path' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+
+		register_rest_route(
+			'peopleapi/v1',
+			'/directory/(?P<id>\d+)',
+			array(
+				'methods' => \WP_REST_Server::READABLE,
+				'callback' => array( __CLASS__, 'get_directory' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+
+		register_rest_route(
+			'peopleapi/v1',
+			'/directory/children/(?P<id>\d+)',
+			array(
+				'methods' => \WP_REST_Server::READABLE,
+				'callback' => array( __CLASS__, 'get_directory_children' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+
+		register_rest_route(
+			'peopleapi/v1',
+			'/directory/descendants/(?P<id>\d+)',
+			array(
+				'methods' => \WP_REST_Server::READABLE,
+				'callback' => array( __CLASS__, 'get_directory_descendants' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+
+	}
+
+
+	/**
+	 * Get Directory
+	 *
+	 * @param \WP_REST_Request $request
+	 * @return \WP_REST_Response
+	 */
+	public static function get_directory( \WP_REST_Request $request ) {
+
+		$directory = array();
+
+		$url_params     = $request->get_url_params();
+		$params         = $request->get_body_params();
+
+		$directory_id   = ( ! empty( $url_params['id'] ) ) ? $url_params['id'] : false;
+
+		if ( $directory_id ) {
+
+			$directory = Directories::get_directory( $directory_id );
+
+		}
+
+		return new \WP_REST_Response( $directory, 200 );
+
+	}
+
+
+	/**
+	 * Get Directory
+	 *
+	 * @param \WP_REST_Request $request
+	 * @return \WP_REST_Response
+	 */
+	public static function get_directory_children( \WP_REST_Request $request ) {
+
+		$children = array();
+
+		$url_params     = $request->get_url_params();
+
+		$directory_id   = ( ! empty( $url_params['id'] ) ) ? $url_params['id'] : false;
+
+		if ( $directory_id ) {
+
+			$children = Directories::get_child_directories( $directory_id, array( 'inherit' => 'children' ) );
+
+		}
+
+		return new \WP_REST_Response( $children, 200 );
+
+	}
+
+
+	/**
+	 * Get Directory
+	 *
+	 * @param \WP_REST_Request $request
+	 * @return \WP_REST_Response
+	 */
+	public static function get_directory_descendants( \WP_REST_Request $request ) {
+
+		$children = array();
+
+		$url_params     = $request->get_url_params();
+
+		$directory_id   = ( ! empty( $url_params['id'] ) ) ? $url_params['id'] : false;
+
+		if ( $directory_id ) {
+
+			$children = Directories::get_child_directories( $directory_id, array( 'inherit' => 'all' ) );
+
+		}
+
+		return new \WP_REST_Response( $children, 200 );
+
+	}
+
+
+	public static function init() {
+
+		add_action( 'rest_api_init', array( __CLASS__, 'register_api_endpoints' ) );
+
+	}
+}
+
+
+Directory_Query::init();
